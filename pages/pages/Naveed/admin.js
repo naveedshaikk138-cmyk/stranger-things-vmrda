3881051ef3ee198128715e03fab90dd3591009d3
naveedshaikk138-cmyk <naveedshaikk138@gmail.com>
2025-12-17 23:54:08 +0530

    Create admin.js

---
+++ pages/pages/Naveed/admin.js
@@ -0,0 +1,17 @@
+export default function Admin() {
+  return (
+    <div style={{
+      backgroundColor: "black",
+      color: "white",
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
+      <h1>🔐 Admin Dashboard</h1>
+      <p>Stranger Things Finale</p>
+      <p>VMRDA Children's Arena</p>
+    </div>
+  );
+}
